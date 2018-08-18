415a76b189a754e93a4ab5cdde86c7fb35c1305f
linweili <295287765@qq.com>
2018-08-19 01:47:40 +0800

    url

--- django_admin/urls.py
+++ django_admin/urls.py
@@ -21,5 +21,5 @@ from ci.jenkins import jenkins_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index', views.test),
-    path('assert_job_exists', jenkins_views.create_job),
+    path('assert_job_exists', jenkins_views.get_job),
 ]
