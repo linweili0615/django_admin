f8734e9f73af4f4403152f1006aff24a8add1065
linweili <295287765@qq.com>
2018-08-19 22:03:43 +0800

    添加axios  并添加代理

--- vue-ci/config/index.js
+++ vue-ci/config/index.js
@@ -10,7 +10,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api':{
+        target : 'http://localhost:8000',
+        changeOrigin : true,
+        pathRewrite : {
+          '^/api' : '/'
+          //写 '/api' 就等于写 'http://localhost:8000'
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
--- vue-ci/src/main.js
+++ vue-ci/src/main.js
@@ -5,10 +5,12 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.prototype.$axios = axios;
 
 /* eslint-disable no-new */
 new Vue({
